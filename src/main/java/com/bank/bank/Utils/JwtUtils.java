a166c33fc770b89d3f5ed5ad01d85fbae9c8aa31
Manu Raj <114493702+manuraj23@users.noreply.github.com>
2026-03-25 21:25:06 +0530

    JWT expiration implement
    Increased JWT expiration time from 10 minutes to 20 minutes.

--- src/main/java/com/bank/bank/Utils/JwtUtils.java
+++ src/main/java/com/bank/bank/Utils/JwtUtils.java
@@ -52,7 +52,7 @@ public class JwtUtils {
                 .header().empty().add("typ", "JWT")
                 .and()
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 10)) // 60 minutes expiration time
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 20)) 
                 .signWith(getSigningKey())
                 .compact();
     }
